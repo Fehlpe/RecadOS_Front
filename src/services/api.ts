1523256a5e416a784bb11e8798a1a9b674b3ba9b
Fehlpe <felpearcanjo@gmail.com>
2023-01-17 14:54:42 -0300

    api from vercel

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8081",
+  baseURL: "https://recados-api.vercel.app",
   headers: {
     "Content-type": "application/json",
   },
